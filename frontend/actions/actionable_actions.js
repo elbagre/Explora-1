485de92f551e6a71b9f1280a4af28517f1b7d7dd
Peter McKinley <peter.michael.mckinley@gmail.com>
2016-12-13 23:31:05 -0500

    Added Actionable Actions

---
+++ frontend/actions/actionable_actions.js
@@ -0,0 +1,23 @@
+export const REQUEST_USER_ACTIONS = "REQUEST_USER_ACTIONS";
+export const RECEIVE_USER_ACTIONS = "RECEIVE_USER_ACTIONS";
+export const CREATE_USER_ACTION = "CREATE_USER_ACTION";
+export const DESTROY_USER_ACTION = "DESTROY_USER_ACTION";
+
+export const requestUserActions = () => ({
+  type: REQUEST_USER_ACTIONS
+});
+
+export const receiveUserActions = (userActions) => ({
+  type: RECEIVE_USER_ACTIONS,
+  user_actions
+});
+
+export const createUserAction = (userAction) => ({
+  type: CREATE_USER_ACTION,
+  user_action
+});
+
+export const destroyUserAction = (actionId) => ({
+  type: DESTROY_USER_ACTION,
+  actionId
+});
